13e4a6a52c5b5f433272b7b2ef89110ae2166189
Scott <dev@misonline.biz>
2014-08-22 00:59:00 -0400

    Updated UMD Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,37 @@
-'use strict'
 
-var gulp = require('gulp')
 
-gulp.task('default', ['lint', 'test'])
+var gulp = require('gulp'),
+    gulputil=require('gulp-util'),
+    path=require('path'),
+    fs = require('fs-extra'),
+    concat=require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    merge = require('merge-stream'),
+    build=require('./build.json'),
+    release=require('./build/dist.json'),
+    src='./src',
+    dist='./dist';
 
-var mocha = require('gulp-mocha')
+
+gulp.task('default', ['lint', 'test']);
+
+var mocha = require('gulp-mocha');
 gulp.task('test', function() {
   return gulp.src(['!test/coverage/**', 'test/**/*.js'], { read: false })
              .pipe(mocha({
                reporter: 'spec'
              }))
-})
+});
 
-var eslint = require('gulp-eslint')
+var eslint = require('gulp-eslint');
 gulp.task('lint', function() {
   return gulp.src(['lib/**/*.js', '!test/coverage/**', 'test/**/*.js', 'gulpfile.js'])
              .pipe(eslint())
              .pipe(eslint.format())
              .pipe(eslint.failOnError())
-})
+});
 
-var istanbul = require('gulp-istanbul')
+var istanbul = require('gulp-istanbul');
 gulp.task('coverage', function(done) {
   gulp.src(['lib/**/*.js'])
       .pipe(istanbul())
@@ -32,10 +43,49 @@ gulp.task('coverage', function(done) {
           }))
           .on('end', done)
       })
-})
+});
 
-var coveralls = require('gulp-coveralls')
+var coveralls = require('gulp-coveralls');
 gulp.task('coveralls', ['coverage'], function() {
   return gulp.src('test/coverage/**/lcov.info')
              .pipe(coveralls())
-})
+});
+
+
+
+
+gulp.task('build',function(){
+
+    var build_=libStream()
+        .pipe(concat('nested-observe.js'))
+        .pipe(gulp.dest(src));
+
+    var release_=releaseStream()
+        .pipe(concat('nested-observe.js'))
+        .pipe(gulp.dest(dist));
+
+    return merge(build_, release_);
+
+});
+
+gulp.task('minify',function(){
+
+    var build_=libStream()
+        .pipe(concat('nested-observe.js'))
+        .pipe(gulp.dest(src));
+
+    var minify_=releaseStream()
+        .pipe(concat('nested-observe.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dist));
+
+    return merge(build_, minify_);
+});
+
+function libStream(){
+    return gulp.src(build);
+}
+
+function releaseStream(){
+    return gulp.src(release);
+}
